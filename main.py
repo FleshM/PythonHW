33dc32e9c19c7b9c3df8676616fa81e59715f36f
Denis <d.pleshivtsev1@gmail.com>
2022-12-03 16:49:36 +0500

    2

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import vacancies
 
 class Program:
     def __init__(self):
+        print('Добро пожаловать!')
         self.type = input('Выберите программу (Вакансии/Статистика): ')
         if self.type == 'Вакансии':
             self.inputConnect = vacancies.InputConnect()
